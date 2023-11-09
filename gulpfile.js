545826b506b5f591c09992956ff787f50aceee47
motorolla29 <eutyou@gmail.com>
2023-11-09 17:09:19 +0300

    ref depencies to use node18 for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 const del = require(`del`);
 const gulp = require(`gulp`);
-const sass = require(`gulp-sass`);
+const sass = require("gulp-sass")(require("sass"));
 const plumber = require(`gulp-plumber`);
 const postcss = require(`gulp-postcss`);
-const autoprefixer = require(`autoprefixer`);
 const server = require(`browser-sync`).create();
 const mqpacker = require(`css-mqpacker`);
 const minify = require(`gulp-csso`);
@@ -16,39 +15,33 @@ const mocha = require(`gulp-mocha`);
 const commonjs = require(`rollup-plugin-commonjs`);
 
 gulp.task(`style`, () => {
-  return gulp.src(`sass/style.scss`).
-    pipe(plumber()).
-    pipe(sass()).
-    pipe(postcss([
-      autoprefixer({
-        browsers: [
-          `last 1 version`,
-          `last 2 Chrome versions`,
-          `last 2 Firefox versions`,
-          `last 2 Opera versions`,
-          `last 2 Edge versions`
-        ]
-      }),
-      mqpacker({sort: true})
-    ])).
-    pipe(gulp.dest(`build/css`)).
-    pipe(server.stream()).
-    pipe(minify()).
-    pipe(rename(`style.min.css`)).
-    pipe(gulp.dest(`build/css`));
+  return gulp
+    .src(`sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([mqpacker({ sort: true })]))
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`sprite`, () => {
-  return gulp.src(`img/sprite/*.svg`)
-  .pipe(svgstore({
-    inlineSvg: true
-  }))
-  .pipe(rename(`sprite.svg`))
-  .pipe(gulp.dest(`build/img`));
+  return gulp
+    .src(`img/sprite/*.svg`)
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
+    .pipe(rename(`sprite.svg`))
+    .pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`scripts`, () => {
-  return gulp.src(`js/main.js`)
+  return gulp
+    .src(`js/main.js`)
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(rollup({}, `iife`))
@@ -57,26 +50,28 @@ gulp.task(`scripts`, () => {
 });
 
 gulp.task(`imagemin`, [`copy`], () => {
-  return gulp.src(`build/img/**/*.{jpg,png,gif}`).
-    pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ])).
-    pipe(gulp.dest(`build/img`));
+  return gulp
+    .src(`build/img/**/*.{jpg,png,gif}`)
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+      ])
+    )
+    .pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`copy-html`, () => {
-  return gulp.src(`*.{html,ico}`).
-    pipe(gulp.dest(`build`)).
-    pipe(server.stream());
+  return gulp
+    .src(`*.{html,ico}`)
+    .pipe(gulp.dest(`build`))
+    .pipe(server.stream());
 });
 
 gulp.task(`copy`, [`copy-html`, `scripts`, `style`, `sprite`], () => {
-  return gulp.src([
-    `fonts/**/*.{woff,woff2}`,
-    `img/*.*`
-  ], {base: `.`}).
-    pipe(gulp.dest(`build`));
+  return gulp
+    .src([`fonts/**/*.{woff,woff2}`, `img/*.*`], { base: `.` })
+    .pipe(gulp.dest(`build`));
 });
 
 gulp.task(`clean`, () => {
@@ -94,7 +89,7 @@ gulp.task(`serve`, [`assemble`], () => {
     notify: false,
     open: true,
     port: 3502,
-    ui: false
+    ui: false,
   });
 
   gulp.watch(`sass/**/*.{scss,sass}`, [`style`]);
@@ -117,13 +112,20 @@ gulp.task(`build`, [`assemble`], () => {
 gulp.task(`test`, function () {
   return gulp
     .src([`js/**/*.test.js`])
-    .pipe(rollup({
-      plugins: [
-        commonjs() // Сообщает Rollup, что модули можно загружать из node_modules
-      ]
-    }, `cjs`)) // Выходной формат тестов — `CommonJS` модуль
+    .pipe(
+      rollup(
+        {
+          plugins: [
+            commonjs(), // Сообщает Rollup, что модули можно загружать из node_modules
+          ],
+        },
+        `cjs`
+      )
+    ) // Выходной формат тестов — `CommonJS` модуль
     .pipe(gulp.dest(`build/test`))
-    .pipe(mocha({
-      reporter: `spec` // Вид в котором я хочу отображать результаты тестирования
-    }));
+    .pipe(
+      mocha({
+        reporter: `spec`, // Вид в котором я хочу отображать результаты тестирования
+      })
+    );
 });
